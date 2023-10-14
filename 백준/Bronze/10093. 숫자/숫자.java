a736dd4cdc860263fb4c4fe80810d6552a6bb566
JINE <113004495+dduinni@users.noreply.github.com>
2023-10-14 21:11:10 +0900

    [Bronze II] Title: 숫자, Time: 1208 ms, Memory: 30724 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10093. 숫자/숫자.java
+++ 백준/Bronze/10093. 숫자/숫자.java
@@ -3,25 +3,18 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-        int A = sc.nextInt();
-        int B = sc.nextInt();
-        int count = 0;
-        if(A<B){
-            for(int i=A+1; i<B; i++){
-                count++;
-            }
-            System.out.println(count);
-            for(int i=A+1; i<B; i++){
+        long A = sc.nextLong();
+        long B = sc.nextLong();
+        long count = (A==B)? 0 : ((B>A) ? B - A - 1 : A - B - 1);
+        System.out.println(count);
+
+        if(B > A) {
+            for (long i = A + 1; i < B; i++) {
                 System.out.print(i + " ");
             }
-        }else{
-            for(int i=B+1; i<A; i++){
-                count++;
-            }
-            System.out.println(count);
-            for(int i=B+1; i<A; i++){
+        } else {
+            for (long i = B + 1; i < A; i++) {
                 System.out.print(i + " ");
             }
         }
