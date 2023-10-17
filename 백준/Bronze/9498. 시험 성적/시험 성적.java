5f8fe0f61f5982f396ea230ea64cc8f4932bc1de
JINE <113004495+dduinni@users.noreply.github.com>
2023-10-17 10:38:30 +0900

    [Bronze V] Title: 시험 성적, Time: 208 ms, Memory: 17672 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -6,13 +6,13 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		int score = sc.nextInt();
 		
-		if(score >= 90) {
+		if(score>=90) {
 			System.out.println("A");
-		}else if(score >= 80) {
+		}else if(score>=80) {
 			System.out.println("B");
-		}else if(score >= 70) {
+		}else if(score>=70) {
 			System.out.println("C");
-		}else if(score >= 60) {
+		}else if(score>=60) {
 			System.out.println("D");
 		}else {
 			System.out.println("F");
