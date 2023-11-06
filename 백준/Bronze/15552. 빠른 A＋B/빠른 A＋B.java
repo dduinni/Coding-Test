37244d6c86f82f57b9d4cd989c44caff6fc8106d
JINE <113004495+dduinni@users.noreply.github.com>
2023-11-06 13:02:15 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 840 ms, Memory: 168808 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i=0; i<N; i++) {
+			String str = br.readLine();
+			int target = str.indexOf(" ");
+			int result = Integer.parseInt(str.substring(0, target)) + Integer.parseInt(str.substring(target+1));
+			sb.append(result+"\n");
+		}
+		br.close();
+		System.out.println(sb);
+	}
+}
